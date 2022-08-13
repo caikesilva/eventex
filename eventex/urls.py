f42fe2fb88d22114d7a611246351b20cb7e7432a
Caike Silva <creis@geobit.com.br>
2022-08-13 00:43:28 -0300

    feat: subscriptions

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from eventex.core.views import home
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('inscricao/', subscribe),
 ]
