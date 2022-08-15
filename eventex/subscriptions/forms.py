da2239aa770902fc3f2451fead6139e856294865
Caike Silva <creis@geobit.com.br>
2022-08-15 18:47:41 -0300

    feat: send mail

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label="Nome", max_length=155)
-    cpf = forms.CharField(label="CPF", max_length=11)
+    cpf = forms.CharField(label="CPF", max_length=14)
     email = forms.EmailField(label="E-mail")
     phone = forms.CharField(label="Telefone", max_length=20)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -17,8 +17,8 @@ def subscribe(request):
             mail.send_mail(
                 'Confirmação de inscrição',
                 body,
-                'contato@eventex.com.br',
-                ['contato@eventex.com.br', form.cleaned_data['email']]
+                'kaykesilva2014.ks@gmail.com',
+                ['kaykesilva2014.ks@gmail.com', form.cleaned_data['email']]
             )
 
             messages.success(request, 'Inscrição realizada com sucesso!')
