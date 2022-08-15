7dbba668ac1167dee6f7ead9662341acb1a818a8
Caike Silva <creis@geobit.com.br>
2022-08-15 19:16:06 -0300

    refactor: importação não utilizada

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import SubscriptionForm
 from django.core import mail
 from django.template.loader import render_to_string
