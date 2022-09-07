295357c8f574f7e76d550fa3edf6b2c9de8f3956
Caike Silva <creis@geobit.com.br>
2022-09-07 18:09:35 -0300

    :sparkles: feat: email ou telefone opcional

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -11,10 +11,16 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label="Nome", max_length=155)
     cpf = forms.CharField(label="CPF", max_length=14, validators=[validate_cpf])
-    email = forms.EmailField(label="E-mail")
-    phone = forms.CharField(label="Telefone", max_length=20)
+    email = forms.EmailField(label="E-mail", required=False)
+    phone = forms.CharField(label="Telefone", max_length=20, required=False)
 
     def clean_name(self):
         name = self.cleaned_data['name']
-        
+
         return name.title()
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        
+        return self.cleaned_data
--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -89,3 +89,13 @@ class SubscriptionsNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_no_field_errors(self):
+        invalid_data = {
+            'name': 'Caike Silva',
+            'cpf': '12345678901',
+        }
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
+
