2214debf6430885b6fb2ad9ac496950be6ce3b6c
Caike Silva <creis@geobit.com.br>
2022-09-07 17:23:52 -0300

    :sparkles: feat: validando cpf

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+def validate_cpf(value):
+    if not value.isdigit():
+        raise ValidationError('CPF deve conter apenas números', 'digits')
+
+    if len(value) != 11:
+        raise ValidationError('CPF deve conter 11 números', 'length')
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label="Nome", max_length=155)
-    cpf = forms.CharField(label="CPF", max_length=14)
+    cpf = forms.CharField(label="CPF", max_length=14, validators=[validate_cpf])
     email = forms.EmailField(label="E-mail")
     phone = forms.CharField(label="Telefone", max_length=20)
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -2,10 +2,45 @@ from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_field(self):
         """Form must have 4 fields"""
+        form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
-        self.assertSequenceEqual(expected, list(self.form.fields))
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_cpf_is_digits(self):
+        """CPF must only accpteds digits"""
+        form = self.make_validated_form(cpf='1234567AB')
+        field = 'cpf'
+        code = 'digits'
+
+        self.assertFormErrorCode(form, field, code)
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits"""
+        form = self.make_validated_form(cpf='1234')
+        field = 'cpf'
+        code = 'length'
+
+        self.assertFormErrorCode(form, field, code)
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def make_validated_form(self, **kwargs):
+        valid = {
+            'name': 'Caike Silva',
+            'cpf': '12345678901',
+            'email': 'cs.caikesilva@gmail.com',
+            'phone': '89-999011434'
+            
+        }
+
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        return form
